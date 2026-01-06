83ba89687ab4187e0f70680ac5b810012ac37e36
T Rock <terence.chia@gmail.com>
2026-01-07 01:27:07 +1100

    recreate miniciv

--- config/frontpage.php
+++ config/frontpage.php
@@ -11,5 +11,5 @@ return [
     | `frontpage.blade.php` file. Set via the `FRONTPAGE_THEME` env var.
     |
     */
-    'theme' => env('FRONTPAGE_THEME', env('FRONTPAGE_THEME', 'default')),
+    'theme' => env('FRONTPAGE_THEME', env('FRONTPAGE_THEME', 'games')),
 ];
