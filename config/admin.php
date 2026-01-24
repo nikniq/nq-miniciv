79853dcc4a52ab1675acb8c04af0fd1911f41911
T Rock <terence.chia@gmail.com>
2026-01-25 06:57:06 +1100

    server dash fix

--- config/admin.php
+++ config/admin.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'servers_enabled' => env('ADMIN_SERVERS_ENABLED', true),
+    'servers_enabled' => env('ADMIN_SERVERS_ENABLED', false),
     'external_logs_enabled' => env('ADMIN_EXTERNAL_LOGS_ENABLED', true),
 ];
